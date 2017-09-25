4bb7a57c38130ad5fe64b12ba73dadb36395399f
Marlon Merjos <marlonmerjos@gmail.com>
2017-09-25 12:12:29 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+// input field of the form that alerts a user when they have pressed the G key.
+//Define a function submitIt that does not accept a parameter. The function should bind a submit event to the form that alerts "".
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(e) {
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
